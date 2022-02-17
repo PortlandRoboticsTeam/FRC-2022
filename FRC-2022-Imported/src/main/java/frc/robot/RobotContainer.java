254a9135658e5eab98b65cd858b8be2dc464a711
twinriversrobotics <twinriversrobotics@gmail.com>
2022-02-17 12:49:00 -0800

    Imported into 2022 and fixed things

---
+++ FRC-2022-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import static frc.robot.Constants.*;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final SwerveShaninigans m_drivetrainSubsystem = new SwerveShaninigans();
+  
+  private final AutoDrive m_autoCommand = new AutoDrive(m_drivetrainSubsystem);
+
+  private final Joystick m_controller = new Joystick(m_controllerPortNum);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+            m_drivetrainSubsystem,
+            () -> -(m_controller.getY() * SwerveShaninigans.MAX_VELOCITY_METERS_PER_SECOND),
+            () -> -(m_controller.getX() * SwerveShaninigans.MAX_VELOCITY_METERS_PER_SECOND),
+            () -> -(m_controller.getTwist() * SwerveShaninigans.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND)
+    ));
+    
+    
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+    // Back button zeros the gyroscope
+    JoystickButton m_1 = new JoystickButton(m_controller, 7);
+
+    m_1.whenPressed(m_drivetrainSubsystem::zeroGyroscope);
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return m_autoCommand;
+  }
+}
