8f423eed2bb8e1d5033e29afefc4e3bde6dbf93e
TwinRiversRobotics <94489274+TwinRiversRobotics@users.noreply.github.com>
2023-02-27 14:35:29 -0500

    Add files via upload

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,140 +4,147 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.commands.*;
-//import frc.robot.commands.ExampleCommand;
-import frc.robot.commands.driving;
-
-//import frc.robot.subsystems.ExampleSubsystem;
-import frc.robot.subsystems.*;
-import frc.robot.subsystems.driveTrain;
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-//import edu.wpi.first.wpilibj.PS4Controller;
-//import edu.wpi.first.wpilibj.GenericHID;
-//import edu.wpi.first.wpilibj.PS4Controller;
+import frc.robot.commands.*;
+import frc.robot.commands.AutoCommands.AutoMoveShootTwo;
+import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj2.command.Command;
-//import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
-import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-//import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import static frc.robot.Constants.*;
 
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
+ * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  //private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
-  private final driveTrain m_driveTrain = new driveTrain();
-  private final wrist m_wrist = new wrist(); 
-  private final elbow m_elbow = new elbow(); 
-  private final shoulder m_Shoulder = new shoulder();
-  public  final driving m_driving ;
-  public final claw m_Claw = new claw();
-  public final wSetPos m_WSetPos = new wSetPos(m_wrist);
-  public final sSetPos m_sSetPos = new sSetPos(m_Shoulder);
-  public final eSetPos m_eSetPos = new eSetPos(m_elbow);
+  private final SwerveShaninigans m_drivetrainSubsystem = new SwerveShaninigans();
+  private final BallGun ballGun = new BallGun();
+  private final FunnyMonkeyMachine funnyMonkeyMachine = new FunnyMonkeyMachine();
 
-  public final Joystick m_Joystick = new Joystick(0);
+  private final AutoMoveShootTwo autoMoveShootTwo = new AutoMoveShootTwo(m_drivetrainSubsystem, ballGun);
+  
+  private final ZeroGyro zeroGyro = new ZeroGyro(m_drivetrainSubsystem);
+  
+  private final ShootBall shootBall = new ShootBall(ballGun, launchSpeed);
+  private final ShootBall retractBall = new ShootBall(ballGun, -launchSpeed);
+  private final ShootBallStop shootBallStop = new ShootBallStop(ballGun);
+  private final SpinHigherConveyor spinHigherConveyor = new SpinHigherConveyor(ballGun, higherConveyorSpeed);
+  private final SpinLowerConveyor spinLowerConveyor = new SpinLowerConveyor(ballGun, lowerConveyorSpeed);
+  private final StopHigherConveyor stopHigherConveyor = new StopHigherConveyor(ballGun);
+  private final StopLowerConveyor stopLowerConveyor = new StopLowerConveyor(ballGun);
+  private final ShootTwoBalls shootTwoBalls = new ShootTwoBalls(ballGun, launchSpeed, higherConveyorSpeed, lowerConveyorSpeed);
+  //private final SendDistance sendDistance = new SendDistance(ballGun);
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandPS4Controller m_driverController =
-      new CommandPS4Controller(OperatorConstants.kDriverControllerPort);
-  //private final PS4Controller ps4 = new PS4Controller(0);
+  private final ExtendArms extendArms = new ExtendArms(funnyMonkeyMachine);
+  private final RetractArms retractArms = new RetractArms(funnyMonkeyMachine);
+  private final CloseArms closeArms = new CloseArms(funnyMonkeyMachine);
+  private final OpenArms openArms = new OpenArms(funnyMonkeyMachine);
+  private final SendPneumatics sendPneumatics = new SendPneumatics(funnyMonkeyMachine);
+  
+  private final Joystick m_controller = new Joystick(m_controllerPortNum);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. 
-   * @param m_driving */
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    m_driveTrain.setDefaultCommand( m_driving = new driving(
-      m_driveTrain,
-      ()->m_driverController.getRawAxis(Constants.driverxAxis),
-      ()->-m_driverController.getRawAxis(Constants.driverYAxis),
-      ()->m_driverController.getRawAxis(Constants.driverRotAxis)
-      ));
+    CameraServer.startAutomaticCapture("Intake", 0);
+    CameraServer.startAutomaticCapture("Launcher", 1);
 
-    //makes the arm constinly update positions  
-    m_wrist.setDefaultCommand(m_WSetPos);
-    m_elbow.setDefaultCommand(m_eSetPos);
-    m_Shoulder.setDefaultCommand(m_sSetPos);
-    // Configure the trigger bindings
-    configureBindings();
+    //Uncomment if using neos and want to throw their position to shuffleboard
+    // Shuffleboard.getTab("Arms").addNumber("rigth arm pos", ()->funnyMonkeyMachine.getRightArmPosition());
+    // Shuffleboard.getTab("Arms").addNumber("left arm pos", ()->funnyMonkeyMachine.getLeftArmPosition());
+
+
+    // Set up the default command for the drivetrain.
+    // The controls are for field-oriented driving:
+    // Left stick Y axis -> forward and backwards movement
+    // Left stick X axis -> left and right movement
+    // Right stick X axis -> rotation
+    m_drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+            m_drivetrainSubsystem,
+            () -> -(smoothLogisticInput(m_controller.getX(), true) * m_controller.getThrottle() * SwerveShaninigans.MAX_VELOCITY_METERS_PER_SECOND * speedReductionConst),
+            () -> (smoothLogisticInput(m_controller.getY(), true) * m_controller.getThrottle() * SwerveShaninigans.MAX_VELOCITY_METERS_PER_SECOND * speedReductionConst),
+            () -> -(smoothLogisticInput(m_controller.getTwist(), false) * m_controller.getThrottle() * SwerveShaninigans.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND * speedReductionConst)
+    ));
     
-  }
-  //set the positons that all the motors go to
-  public void setAllPos(int pos){
-    m_wrist.position =pos;
-    m_elbow.position =pos;
-    m_Shoulder.position =pos;
-  }
-  public boolean getAllAtHome(){
-    if (m_wrist.atHome&&m_elbow.atHome&&m_Shoulder.atHome){
-      return true;
-    }else{
-      return false;
-    }
+    // Configure the button bindings
+    configureButtonBindings();
+    //SmartDashboard.putData(sendDistance);
+    SmartDashboard.putData(sendPneumatics);
   }
 
-  public void setAllAtHome(boolean atHome){
-    m_wrist.atHome = atHome;
-    m_elbow.atHome = atHome;
-    m_Shoulder.atHome = atHome;
-  }
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
+  private void configureButtonBindings() {
+    JoystickButton m_2 = new JoystickButton(m_controller, 2);
+    JoystickButton m_3 = new JoystickButton(m_controller, 3);
+    JoystickButton m_4 = new JoystickButton(m_controller, 4);
+    JoystickButton m_6 = new JoystickButton(m_controller, 6);
+    JoystickButton m_1 = new JoystickButton(m_controller, 1);
+    JoystickButton m_5 = new JoystickButton(m_controller, 5);
+    JoystickButton m_7 = new JoystickButton(m_controller, 7);
+    JoystickButton m_8 = new JoystickButton(m_controller, 8);
+    JoystickButton m_9 = new JoystickButton(m_controller, 9);
+    JoystickButton m_10 = new JoystickButton(m_controller, 10);
+    JoystickButton m_11 = new JoystickButton(m_controller, 11);
 
-  private void configureBindings() {
-    //  Trigger xButton = m_driverController.cross();
-    //  Trigger oButton = m_driverController.circle();
-    //  Trigger triButton = m_driverController.triangle();
-    //  Trigger sqrButton  = m_driverController.square();
-    //sets positions
-    if (getAllAtHome()){
-      if (m_Joystick.getRawButton(1)){
-        setAllPos(1);
-        setAllAtHome(false);
-      }else if (m_Joystick.getRawButton(2)){
-        setAllPos(2);
-        setAllAtHome(false);
-      }else if (m_Joystick.getRawButton(3)){
-        setAllPos(3);
-        setAllAtHome(false);
-      }else if (m_Joystick.getRawButton(4)){
-        setAllPos(4);
-        setAllAtHome(false);
-      }else if (m_Joystick.getRawButton(5)){
-        setAllPos(5);
-        setAllAtHome(false);
-      }else if (m_Joystick.getRawButton(6)){
-        setAllPos(6);
-        setAllAtHome(false);
-      }
-    }else if(m_Joystick.getRawButton(0)){
-      setAllPos(0);
-      setAllAtHome(true);
-    }
-    if (m_Joystick.getRawButton(0)){
-      m_Claw.openClose();
-    }
+    m_2.whenPressed(retractBall);
+    m_2.whenReleased(shootBallStop);
+    //m_5.whenPressed(shootTwoBalls);
+    m_3.whenPressed(zeroGyro);
+    m_4.whenPressed(spinLowerConveyor);
+    m_6.whenPressed(spinHigherConveyor);
+    m_1.whenPressed(shootBall);
+    m_4.whenReleased(stopLowerConveyor);
+    m_6.whenReleased(stopHigherConveyor);
+    m_1.whenReleased(shootBallStop);
+    m_7.whileHeld(extendArms);
+    m_8.whileHeld(retractArms);
+    m_9.whenPressed(openArms);
+    m_10.whenPressed(closeArms);
+    //Don't run this command without neos or encoders, shit will break.
+    //m_11.whenPressed(new SequentialCommandGroup(new OpenArms(funnyMonkeyMachine), new ExtendArms(funnyMonkeyMachine), new CloseArms(funnyMonkeyMachine), new RetractArms(funnyMonkeyMachine)));
   }
 
-/**
+  /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return null;
+    // An ExampleCommand will run in autonomous
+    return autoMoveShootTwo;
+  }
+
+  public double smoothLogisticInput(double input, Boolean drive) {
+    if(drive){
+      if (input > 0.1) {
+          return 1/(1 + Math.exp(-10*Math.abs(input)+5));
+      } else if (input < -0.1) {
+          return -(1/(1 + Math.exp(-10*Math.abs(input)+5)));
+      } 
+      return 0;
+    }
+    else{
+      if (input > 0.4) {
+        return 1/(1 + Math.exp(-15*Math.abs(input)+10));
+    } else if (input < -0.4) {
+        return -(1/(1 + Math.exp(-15*Math.abs(input)+10)));
+    } 
+    return 0;
+    }
   }
 }
